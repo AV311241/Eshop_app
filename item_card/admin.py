762e116c50e4848a3ed7b848ca9660f1b1c7aad5
Abhishek verma <86115498+AV311241@users.noreply.github.com>
2022-03-29 10:14:34 +0530

    eshop

---
+++ item_card/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+class Item_cardAdmin(admin.ModelAdmin):
+    list_display = ('id','img_title','img_img',)
+
+admin.site.register(Item_card,Item_cardAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','id')
+
+admin.site.register(Category,CategoryAdmin)
+
+
+admin.site.register(Customer)
+admin.site.register(Order)
